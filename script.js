4c87e23f9ba175d7af3b8644482fe793c333a584
Van Nguyen <v.vannt34@vinid.net>
2021-05-06 19:14:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("RUN SCRIPT VanNT");
+
+// Đổi style của tiêu đề Our companies thành chữ in đậm và kích thước 32px
+// Gợi ý: Sử dụng .style.fontWeight và .style.fontSize
+document.getElementsByTagName("h2")[0].style.fontWeight = "bold";
+document.getElementsByTagName("h2")[0].style.fontSize = "32px";
+
+// Đổi màu nền của phần footer thành màu #0984e3
+// Gợi ý: Sử dụng .style.backgroundColor
+document.getElementsByTagName("footer")[0].style.backgroundColor = "#0984e3";
+
+// Đổi đoạn text ở footer từ "© 2020 One Mount Group, JSC." thành "© 2021 One Mount Group, JSC."
+// Gợi ý: Sử dụng .innerText
+document.querySelectorAll("p.footer-textarea")[2].innerText = "© 2021 One Mount Group, JSC.";
+
+// Đổi ảnh đầu tiên thành ảnh onhousing.png
+// Gợi ý: Sử dụng .src
+document.getElementsByTagName("img")[0].src = "img/onhousing.png";
